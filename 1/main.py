177a8c751416dac2b2b9d8967e091e8f0603c706
Godswillben <amogugodswill99@gmail.com>
2023-08-08 16:55:08 +0100

    fastapi basics

--- 1/main.py
+++ 1/main.py
@@ -12,7 +12,10 @@ def index():
 # def get_all_blogs():
 #     return {'message': 'All blogs provided'}
 
-@app.get('/blog/all')
+@app.get('/blog/all', 
+         tags=['blog'], 
+         summary="Retrieve all blogs", 
+         description="This api call simulates fetching all blogs")
 def get_all_blogs(page, page_size: Optional[int]=None):
     return {'message':f"All {page_size} blogs on page {page}"}
 
@@ -21,11 +24,11 @@ class BlogType(str, Enum):
     story = 'story'
     howto = 'howto'
 
-@app.get("/blog/type/{type}")
+@app.get("/blog/type/{type}", tags=['blog'])
 def get_blog_type(type: BlogType):
     return {'message':f"Blog Type {type}"}
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
 def get_blog(id: int, response:Response):
     if id > 5:
         return {'error':f'Blog {id} not found'}
