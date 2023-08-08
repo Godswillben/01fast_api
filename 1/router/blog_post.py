7f44568e611bdc28c3be9eb881b832392ddbedae
Godswillben <amogugodswill99@gmail.com>
2023-08-08 21:00:22 +0100

    fastapi basics

--- 1/router/blog_post.py
+++ 1/router/blog_post.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from pydantic import BaseModel
 
 router = APIRouter(
@@ -12,6 +12,22 @@ class BlogModel(BaseModel):
     content: str
     published: Optional[bool]
 
-@router.post('/new')
-def create_blog(blog: BlogModel):
-    return {'data':BlogModel}
+@router.post('/new/{id}')
+def create_blog(blog: BlogModel, id: int, version: int=1):
+    return {
+        'id':id,
+        'data': blog,
+        'version': version
+    }
+
+@router.post('/new/{id}/comment')
+def create_comment(blog: BlogModel, id: int, 
+        comment_id: int = Query(None, title="id of the comment", 
+               description="Some description for comment_id", 
+               alias="commentID", 
+               deprecated=True)):
+    return {
+        'blog':blog,
+        'id': id,
+        'comment_id': comment_id
+    }
