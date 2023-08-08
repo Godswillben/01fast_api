9ad38ab9ff1b0f9e4dd4e1f06ee68f471e49459f
Godswillben <amogugodswill99@gmail.com>
2023-08-08 18:12:21 +0100

    fastapi basics

--- 1/main.py
+++ 1/main.py
@@ -1,36 +1,7 @@
-from typing import Optional
-from fastapi import FastAPI, Response, status
-from enum import Enum
+from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get('/')
 def index():
     return {"message" : 'Hello World!'}
-
-# @app.get('/blog/all')
-# def get_all_blogs():
-#     return {'message': 'All blogs provided'}
-
-@app.get('/blog/all', 
-         tags=['blog'], 
-         summary="Retrieve all blogs", 
-         description="This api call simulates fetching all blogs")
-def get_all_blogs(page, page_size: Optional[int]=None):
-    return {'message':f"All {page_size} blogs on page {page}"}
-
-class BlogType(str, Enum):
-    short = 'short'
-    story = 'story'
-    howto = 'howto'
-
-@app.get("/blog/type/{type}", tags=['blog'])
-def get_blog_type(type: BlogType):
-    return {'message':f"Blog Type {type}"}
-
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
-def get_blog(id: int, response:Response):
-    if id > 5:
-        return {'error':f'Blog {id} not found'}
-    return {'message':f"Blog with id {id}"}
-
---
+++ 1/router/blog_get.py
@@ -0,0 +1,38 @@
+from enum import Enum
+from fastapi import APIRouter, Response, status
+from typing import Optional
+
+
+router = APIRouter(
+    prefix='/blog',
+    tags=['blog']
+)
+
+
+# @app.get('/blog/all')
+# def get_all_blogs():
+#     return {'message': 'All blogs provided'}
+
+@router.get('/all', 
+         tags=['blog'], 
+         summary="Retrieve all blogs", 
+         description="This api call simulates fetching all blogs")
+def get_all_blogs(page, page_size: Optional[int]=None):
+    return {'message':f"All {page_size} blogs on page {page}"}
+
+class BlogType(str, Enum):
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
+
+@router.get("/type/{type}", tags=['blog'])
+def get_blog_type(type: BlogType):
+    return {'message':f"Blog Type {type}"}
+
+@router.get('/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
+def get_blog(id: int, response:Response):
+    if id > 5:
+        return {'error':f'Blog {id} not found'}
+    return {'message':f"Blog with id {id}"}
+
+
